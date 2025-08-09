9493294bdb8e6d8c7ee4ed868031674ad5311870
ryan <sweetplastik@gmail.com>
2025-08-10 06:57:02 +0900

    bug 1

--- app/src/main/java/com/example/alcoholictimer/RecordsActivity.kt
+++ app/src/main/java/com/example/alcoholictimer/RecordsActivity.kt
@@ -11,8 +11,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
