dc5c3e94c9cabb58063917df6328b54b69374915
ryan <sweetplastik@gmail.com>
2025-11-17 04:18:55 +0900

    chore(records): delete moved RecordsScreen wrapper file

--- app/src/main/java/kr/sweetapps/alcoholictimer/feature/records/components/RecordsScreen.kt
+++
@@ -1,6 +0,0 @@
-package kr.sweetapps.alcoholictimer.feature.records.components
-
-// 이 파일은 더 이상 사용되지 않습니다.
-// RecordsScreen 구현은 다음 새 위치로 이동했습니다:
-// kr.sweetapps.alcoholictimer.ui.screens.RecordsScreen
-// 원본 파일을 안전하게 비우고 참조는 모두 새 위치로 교체되었습니다.
