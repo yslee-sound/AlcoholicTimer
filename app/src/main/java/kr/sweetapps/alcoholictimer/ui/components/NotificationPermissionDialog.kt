713d35fa3b76848004ccad994aca654f420be4cb
dev <dev@example.com>
2026-01-03 11:18:22 +0900

    rel 1.1.8 retention 0.17

--- app/src/main/java/kr/sweetapps/alcoholictimer/ui/components/NotificationPermissionDialog.kt
+++ app/src/main/java/kr/sweetapps/alcoholictimer/ui/components/NotificationPermissionDialog.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.tooling.preview.Preview
 import kr.sweetapps.alcoholictimer.R
 
 /**
@@ -122,3 +123,14 @@ fun NotificationPermissionDialog(
     }
 }
 
+/**
+ * NotificationPermissionDialog Preview
+ */
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+fun NotificationPermissionDialogPreview() {
+    NotificationPermissionDialog(
+        onConfirm = { /* Preview - do nothing */ },
+        onDismiss = { /* Preview - do nothing */ }
+    )
+}
